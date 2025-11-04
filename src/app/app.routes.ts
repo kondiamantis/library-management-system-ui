60573cb12ef39cc9b8b1f7326d29389c529b6ae5
Persefoni Koutridi <koutridi.persefoni@gmail.com>
2025-11-04 22:24:02 +0200

    fixed config for http client and routes for components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Books } from './features/books/books';
+import { Members } from './features/members/members';
+import { Borrowings } from './features/borrowings/borrowings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'books', component: Books },
+    { path: 'members', component: Members },
+    { path: 'borrowings', component: Borrowings },
+  ];
